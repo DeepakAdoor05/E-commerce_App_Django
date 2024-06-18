eabd8fb0199298abff4502f49910f8432fbd8e83
Deepak <deepak@gmail.com>
2024-06-18 22:09:17 +0530

    Model for orders commpleted

--- brokart/orders/models.py
+++ brokart/orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from customers.models import Customer
+from products.models import product
+# Model for orders.
 
-# Create your models here.
+# Data Model for order.
+class Orders(models.Model):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
+    # Stages of order.
+    CART_STAGE=0
+    ORDER_CONFIRMED=1
+    ORDER_PROCESSED=2
+    ORDER_DELIVERED=3
+    ORDER_REJECTED=4
+    STATUS_CHOICE=((ORDER_PROCESSED,"ORDER_PROCESSED")
+                   (ORDER_DELIVERED,"ORDER_DELIVERED")
+                   (ORDER_REJECTED,"ORDER_REJECTED")
+                   )
+    order_status=models.IntegerField(choices=STATUS_CHOICE,default=CART_STAGE)
+    owner=models.ForeignKey(Customer,on_delete=models.SET_NULL,related_name='orders')
+    delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    deleted_at=models.DateTimeField(auto_now=True)
+
+# Model for ordered item
+class Ordered_Item(models.Model):
+    product=models.ForeignKey(product,related_name='addaed_carts',on_delete=models.SET_NULL)
+    quantity=models.IntegerField(default=1)
+    owner=models.ForeignKey(Orders,related_name='added_items',on_delete=models.CASCADE)
