ef4cc0f6ca87f96f085367624a810a5543a20ac2
Deepak <deepak@gmail.com>
2024-06-18 22:13:05 +0530

    themes model completed

--- brokart/themes/models.py
+++ brokart/themes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Site_setting(models.Model):
+    banner=models.ImageField(upload_to='media/site/')
+    caption=models.TextField()
