af3017f9c67fd0d1ddfe1565767933e2d71609f6
Deepak <deepak@gmail.com>
2024-06-18 20:49:03 +0530

    Model for customer commpleted - user's 'u' written small

--- brokart/customers/models.py
+++ brokart/customers/models.py
@@ -8,7 +8,7 @@ class Customer(models.Model):
     DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
     name=models.CharField(max_length=200)
     address=models.TextField()
-    user=models.OneToOneField(user,on_delete=models.CASCADE,related_name='customer_profile')
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer_profile')
     phone=models.CharField(max_length=10)
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at=models.DateTimeField(auto_now_add=True)
