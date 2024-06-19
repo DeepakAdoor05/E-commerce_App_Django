81ba2cb31d409585f29d07e1bb9bd5b263ff2b35
Deepak <deepak@gmail.com>
2024-06-19 13:21:27 +0530

    urls.py added for products

--- brokart/brokart/settings.py
+++ brokart/brokart/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-SSTATIC_URL = 'brocart_files/'
+STATIC_URL = 'brocart_files/'
 STATICFILES_DIRS =[
     BASE_DIR/ "static",
 ]
