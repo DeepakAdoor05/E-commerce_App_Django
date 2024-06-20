7312c37838098d414210a0f400cae6c9f60cefbd
Deepak <deepak@gmail.com>
2024-06-20 19:35:54 +0530

    product work completed upto a single product detail

--- brokart/products/urls.py
+++ brokart/products/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index,name='home'),
     path('product_list',views.list_products,name='list_products'),
+    path('product_details',views.detail_product,name='detail_product'),
 ]
