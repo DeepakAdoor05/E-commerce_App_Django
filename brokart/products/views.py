17836fe21addda837bc2c8ebbc650d312925f0e1
Deepak <deepak@gmail.com>
2024-06-22 14:57:01 +0530

    The products are arranged in products page based on assigned priorities

--- brokart/products/views.py
+++ brokart/products/views.py
@@ -10,11 +10,13 @@ def list_products(request):
     page=1
     if request.GET:
         page=request.GET.get('page',1)
-    product_list=product.objects.all()
+    product_list=product.objects.order_by('priority')
     product_paginator=Paginator(product_list,8)
     product_list=product_paginator.get_page(page)
     context={'products':product_list}
     return render(request,'products.html',context)
 
-def detail_product(request):
-    return render(request,'product_detail.html')
+def detail_product(request,pk):
+    Product=product.objects.get(pk=pk)
+    context={'product':Product}
+    return render(request,'product_detail.html',context)
