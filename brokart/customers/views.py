1c72a65e18da6b290b0b69c9dcd1a415a7f8bc0f
Deepak <deepak@gmail.com>
2024-06-23 00:00:45 +0530

    Account management started and worked on register form in account page

--- brokart/customers/views.py
+++ brokart/customers/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Customer
 # Create your views here.
 
 # Function for SignUp and SignIn.
 def show_account(request):
+    if request.POST and 'register' in request.POST:
+        try:
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            email=request.POST.get('email')
+            address=request.POST.get('address')
+            phone=request.POST.get('phone')
+            # Create user accounts.
+            user=User.objects.create(
+                username=username,
+                password=password,
+                email=email
+                )
+            # Create customer account.
+            customer=Customer.objects.create(
+                user=user,
+                phone=phone,
+                address=address
+            )
+            return redirect('home')
+        except Exception as e:
+            error_message="Duplicate username or invalid credentials"
+            messages.error(request,error_message)
     return render(request,'account.html')
