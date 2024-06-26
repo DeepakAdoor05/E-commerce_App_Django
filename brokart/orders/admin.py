d1222e3cadce2df952db5f211bc1ac46bd9c2963
Deepak <deepak@gmail.com>
2024-06-26 18:21:34 +0530

    Added Filters in admin page

--- brokart/orders/admin.py
+++ brokart/orders/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Orders,Ordered_Item
 # Register your models here.
+# This the class which helps us to customize the default admin given by Orders app
+class OrderAdmin(admin.ModelAdmin):
+    list_filter=[
+        "owner",
+        "order_status",
+    ]
+    search_fields=(
+        "owner",
+        "id"
+    )
 
-admin.site.register(Orders)
+admin.site.register(Orders,OrderAdmin)
